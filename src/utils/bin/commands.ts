b35319f7d128c505d3d48406423ffc229e548cd9
VARUN1128 <varunharidas@gmail.com>
2025-07-18 21:27:37 +0530

    Fix donate command for Vercel deploy

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -42,11 +42,7 @@ export const resume = async (args: string[]): Promise<string> => {
 
 // Donate
 export const donate = async (args: string[]): Promise<string> => {
-  return `thank you for your interest. 
-here are the ways you can support my work:
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
-`;
+  return `Thank you for your interest. Donations are not accepted at this time.`;
 };
 
 // Contact
